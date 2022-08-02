64177e434a9d3e0429a7ff0e6e50c5eaa1088fcf
wacho91 <criskol.71@gmail.com>
2022-08-02 13:40:07 -0500

    Update

--- db.js
+++ db.js
@@ -2,17 +2,9 @@ const owners = require("./data/owners")
 const users = require("./data/users")
 const padelFields = require("./data/padelFields")
 
-// const posts = require("./data/posts")
-// const comments = require("./data/comments")
-
-// const questions = require("./data/questions")
 
 module.exports = {
     owners,
     users,
     padelFields
-
-    // posts,
-    // comments,
-    // questions
 }
