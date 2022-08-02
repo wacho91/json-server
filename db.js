0ed13dbbb42b02d7a017721396f5e43c49c4c9c0
wacho91 <criskol.71@gmail.com>
2022-08-02 13:38:27 -0500

    Json-server-padel

---
+++ db.js
@@ -0,0 +1,18 @@
+const owners = require("./data/owners")
+const users = require("./data/users")
+const padelFields = require("./data/padelFields")
+
+// const posts = require("./data/posts")
+// const comments = require("./data/comments")
+
+// const questions = require("./data/questions")
+
+module.exports = {
+    owners,
+    users,
+    padelFields
+
+    // posts,
+    // comments,
+    // questions
+}
